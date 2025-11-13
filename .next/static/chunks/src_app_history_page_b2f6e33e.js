7f55e6f445b575c5cdc946241cd5e904ab7a9bf5
Shiva1310 <115202307+Shiva1310@users.noreply.github.com>
2025-11-13 17:45:48 +0530

    Delete .next directory

--- .next/static/chunks/src_app_history_page_b2f6e33e.js
+++
@@ -1,8 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_next_b49fb8fa._.js",
-  "static/chunks/src_app_Components_History_History_jsx_68d925e0._.js"
-],
-    source: "dynamic"
-});
